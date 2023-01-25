fbd987e9d5bd47f94c2e6c6f5ec262a947f87450
vicson <vicsonabuka@gmail.com>
2023-01-25 16:23:28 +0100

    Add the pricing page

--- src/Pricing.js
+++ src/Pricing.js
@@ -1,29 +1,35 @@
+import './Pricing.css'
 export default function Pricing() {
     return(
-        <div>
-            <div>
-                <h3>Enterprise</h3>
-            <p>includes live training and expanded useage rights</p>
-             <h3>$799</h3>
-             <p>Per Year</p>
-             <button>Order Now</button>
+        <div className='price_category'>
+            <div className='category1'>
+                <li id='header'>Enterprise</li>
+            <li id='subtitle'>includes live training and expanded useage rights</li>
             </div>
-            <div>
-                <h3>Self-Managed</h3>
-            <p>All platform features no support included</p>
-             <h3>$199</h3>
-             <p>Per Year</p>
-             <button>Order Now</button>
+            <div className='price1'>
+             <li id='header'>$799</li>
+             <li id='subtitle'>Per Year</li>
             </div>
-            <div>
-                <h3>Startup</h3>
-            <p>includes support ticket and all platform features</p>
-             <h3>$499</h3>
-             <p>Per Year</p>
-             <button>Order Now</button>
+            <div className='regbtn'><li><a href='/registration'>Order Now</a></li></div>
+            <div className='category2'>
+                <li id='header'>Self-Managed</li>
+            <li id='subtitle'>All platform features no support included</li>
+             </div>
+             <div className='price2'>
+             <li id='header'>$199</li>
+             <li id='subtitle'>Per Year</li>
             </div>
-            <div></div>
+            <div className='regbtn'><li><a href='/registration'>Order Now</a></li></div>
+            <div className='category3'>
+                <li id='header'>Startup</li>
+            <li id='subtitle'>includes support ticket and all platform features</li>
+             </div>
+             <div className='price3'>
+             <li id='header'>$499</li>
+             <li id='subtitle'>Per Year</li>
+            </div>
+            <div className='regbtn'><li><a href='/registration'>Order Now</a></li></div>
         </div>
         
     )
-}
+};
