3ac6d6e9c3b2900e8569abbb69b1d1b1d4212f4d
vicson <vicsonabuka@gmail.com>
2023-01-30 18:01:54 +0100

    Add the about page file

---
+++ src/pages/About.js
@@ -0,0 +1,36 @@
+//import '/About.css';
+
+export default function About () {
+    return (
+        <div className='team'>
+            <h3>Meet our team</h3>
+            <div className='team__control'>
+            <h5>Efosa Enoma</h5>
+            <img src='' />
+            <h5>Founder & CEO</h5>
+            <p>Enterpreneur and strategy maintains key strategic relationships with clients. M.A international Political Economics and Development.</p>
+            </div>
+            
+            <div className='team__control'>
+            <h5>Jeremiah Alufua</h5>
+            <img src='' />
+            <h5>Developer</h5>
+            <p>Enterpreneur and strategy maintains key strategic relationships with clients. M.A international Political Economics and Development.</p>
+            </div>
+
+            <div className='team__control'>
+            <h5>Osakpolor</h5>
+            <img src='' />
+            <h5>Human Capital Devt. and Learning</h5>
+            <p>Enterpreneur and strategy maintains key strategic relationships with clients. M.A international Political Economics and Development.</p>
+            </div>
+
+            <div className='team__control'>
+            <h5>Oladotun Ajao</h5>
+            <img src='' />
+            <h5>Advisor</h5>
+            <p>Enterpreneur and strategy maintains key strategic relationships with clients. M.A international Political Economics and Development.</p>
+            </div>
+        </div>
+    )
+}
