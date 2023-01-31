3a14fed92e51eb52b52680139aec22739e0322f0
vicson <vicsonabuka@gmail.com>
2023-01-31 18:58:27 +0100

    Add update

--- src/pages/About.js
+++ src/pages/About.js
@@ -1,36 +1,53 @@
-//import '/About.css';
+import './About.css';
+import img from './images/efosa.jpg'
 
 export default function About () {
     return (
-        <div className='team'>
-            <h3>Meet our team</h3>
+        <div>
+            <div className='wrapper'>
+         <h1 className='aheader'>Meet our team</h1>
+
+            <div className='team'>
             <div className='team__control'>
-            <h5>Efosa Enoma</h5>
-            <img src='' />
-            <h5>Founder & CEO</h5>
+            <img src={img} alt='images'/>
+            
+            <br></br>
+            <h3>Efosa Enoma</h3>
+            <p className='role'>Founder & CEO</p>
             <p>Enterpreneur and strategy maintains key strategic relationships with clients. M.A international Political Economics and Development.</p>
             </div>
             
             <div className='team__control'>
-            <h5>Jeremiah Alufua</h5>
-            <img src='' />
-            <h5>Developer</h5>
+         
+            <img src={img}  alt='images'/>
+            <br></br>
+            <h3>Jeremiah Alufua</h3>
+            <p className='role'>Developer</p>
             <p>Enterpreneur and strategy maintains key strategic relationships with clients. M.A international Political Economics and Development.</p>
+           
             </div>
 
             <div className='team__control'>
-            <h5>Osakpolor</h5>
-            <img src='' />
-            <h5>Human Capital Devt. and Learning</h5>
+          
+            <img src={img} alt='images' />
+            <br></br>
+            <h3>Osakpolor</h3>
+            <p className='role'>Human Capital Devt. and Learning</p>
             <p>Enterpreneur and strategy maintains key strategic relationships with clients. M.A international Political Economics and Development.</p>
+          
             </div>
 
             <div className='team__control'>
-            <h5>Oladotun Ajao</h5>
-            <img src='' />
-            <h5>Advisor</h5>
+            
+            <img src={img} alt='images' />
+            <br></br>
+            <h3>Oladotun Ajao</h3>
+            <p className='role'>Advisor</p>
             <p>Enterpreneur and strategy maintains key strategic relationships with clients. M.A international Political Economics and Development.</p>
+          
             </div>
+        </div>
+        </div>
         </div>
     )
 }
