e8f56ac281dc28a40ccbe94141537cd8c8845e08
vicson <vicsonabuka@gmail.com>
2023-02-01 16:28:54 +0100

    Add a new file for account

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import NavBar from "./NavBar";
 import Home from "./pages/Home";
 import Pricing from "./pages/Pricing";
 import About from "./pages/About"
+import Account from "./pages/Account";
 import {Route, Routes} from "react-router-dom";
 function App() {
 
@@ -13,6 +14,7 @@ function App() {
         <Route path="/" element={<Home />} />
         <Route path="/pricing" element={<Pricing />} />
         <Route path="/about" element={<About />} />
+        <Route path="/account" element={<Account />} />
       </Routes>
     </div>
     </>
