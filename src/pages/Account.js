d756448d5179bd8c712e2beaf52888c7984c94c4
vicson <vicsonabuka@gmail.com>
2023-02-01 16:37:07 +0100

    Add new file

---
+++ src/pages/Account.js
@@ -0,0 +1,21 @@
+export default function Account () {
+return (
+    <>
+    <form>
+        <input placeholder="Enter your email" />
+        <br/>
+        <input placeholder="First name" />
+        <br/>
+        <input placeholder="Last name" />
+        <br/>
+        <input placeholder="Company name" />
+        <br/>
+        <input placeholder="Country/Region" />
+        <br/>
+        <input placeholder="Postcode/Zip(optional)" />
+        <br/>
+        <input placeholder="Create password"/>
+    </form>
+    </>
+);
+}
