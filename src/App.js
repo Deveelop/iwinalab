fb80a9eea63907efcf77b559965b74355059abe6
vicson <vicsonabuka@gmail.com>
2023-02-04 18:08:55 +0100

    Add edit routes component

--- src/App.js
+++ src/App.js
@@ -2,8 +2,9 @@ import NavBar from "./NavBar";
 import Home from "./pages/Home";
 import Pricing from "./pages/Pricing";
 import About from "./pages/About"
-import Account from "./pages/Account";
+
 import {Route, Routes} from "react-router-dom";
+import SignIn from "./pages/SignIn";
 function App() {
 
   return (
@@ -14,7 +15,7 @@ function App() {
         <Route path="/" element={<Home />} />
         <Route path="/pricing" element={<Pricing />} />
         <Route path="/about" element={<About />} />
-        <Route path="/account" element={<Account />} />
+        <Route path="/signin" element={<SignIn />} />
       </Routes>
     </div>
     </>
